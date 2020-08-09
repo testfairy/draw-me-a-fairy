e92604234d5691fa2e8971b0b0adcdaf0350737b
Gil Megidish <gil@megidish.net>
2020-08-09 15:44:06 +0300

    Changed http network request to a more meaningful one

--- app/src/main/java/com/testfairy/samples/drawmefairy/DrawingActivity.java
+++ app/src/main/java/com/testfairy/samples/drawmefairy/DrawingActivity.java
@@ -40,8 +40,10 @@ import colorpicker.ColorPickerDialog;
 import colorpicker.ColorPickerDialog.OnColorSelectedListener;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 import utils.ActivityTime;
 
@@ -146,7 +148,10 @@ public class DrawingActivity extends Activity {
 			.build();
 
 		Request request = new Request.Builder()
-			.url("https://www.testfairy.com/_drawmefairy/painter-launched")
+			.url("https://www.testfairy.com")
+			.method("POST", RequestBody.create(MediaType.parse("application/text"), "key1=value1"))
+			.addHeader("Cookie", "source=draw-me-a-fairy")
+			.addHeader("X-Platform", "Android")
 			.build();
 		client.newCall(request).enqueue(new Callback() {
 			@Override public void onFailure(Call call, IOException e) {}
